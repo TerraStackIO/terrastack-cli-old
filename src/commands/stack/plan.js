4bf61f42da833451b934c365cb68e1f4b55a86e6
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-10-16 23:25:20 +0200

    Refactoring to adpat to changes in core

--- src/commands/stack/plan.js
+++ src/commands/stack/plan.js
@@ -5,14 +5,53 @@
  */
 
 const { Command, flags } = require("@oclif/command");
+const { Terrastack } = require("terrastack");
+const log4js = require("log4js");
+const chalk = require("chalk");
+
+log4js.configure({
+  appenders: {
+    multi: {
+      type: "multiFile",
+      base: "logs/",
+      property: "categoryName",
+      extension: ".log"
+    }
+  },
+  categories: {
+    default: { appenders: ["multi"], level: "debug" }
+  }
+});
+
+let loggers = {};
 
 class PlanCommand extends Command {
   async run() {
     const { flags } = this.parse(PlanCommand);
     const stack = require(process.cwd() + "/stack.js");
+    const terrastack = new Terrastack(stack);
+    let buffer = [];
+
+    terrastack.events.on("component:before", function(component) {
+      loggers[component.name] = log4js.getLogger(component.name);
+    });
+
+    terrastack.events.on("output:*", function(component, output) {
+      loggers[component.name].info(output.trim());
+      buffer.push(output);
+    });
+
+    terrastack.events.on("component:**", function(component) {
+      console.log(`${component.name}: ${this.event}`);
+    });
+
+    terrastack.events.on("error", function(component) {
+      console.log(chalk.red.bold.underline(`Error: ${component.name}`));
+      console.log(`Recent output: ${buffer.join("")}`);
+    });
 
     (async () => {
-      await stack.plan();
+      await terrastack.plan();
     })();
   }
 }
